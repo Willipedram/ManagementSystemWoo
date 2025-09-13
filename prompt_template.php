eea1337a0b1e22220fb859afb5e2349cffbd0326
Pedram <32197057+Willipedram@users.noreply.github.com>
2025-09-13 08:38:10 +0330

    Add logging and flags for price editor

--- prompt_template.php
+++ prompt_template.php
@@ -1,6 +1,42 @@
 <?php
-function build_prompt(array $vars){
+function build_prompt(array $vars, array $categories = [], string $content = ''){
     $template = file_get_contents(__DIR__.'/prompt_template.txt');
+    list($db,$prefix) = connect_local_prompt();
+    if($db){
+        $il='';
+        $catNames = array_map(function($c){ return mb_strtolower($c['name']); }, $categories);
+        $plainContent = mb_strtolower(strip_tags($content));
+        $res=$db->query("SELECT title,url FROM {$prefix}internal_links");
+        if($res){
+            while($r=$res->fetch_assoc()){
+                $title=$r['title'];
+                $lower = mb_strtolower($title);
+                $include = in_array($lower,$catNames) || mb_stripos($plainContent,$title)!==false;
+                if($include){ $il.="<li><a href=\"{$r['url']}\">{$title}</a></li>"; }
+            }
+            $res->close();
+        }
+        $vars['{INTERNAL_LINKS}']=$il;
+        $el='';
+        $res=$db->query("SELECT title,url FROM {$prefix}external_links");
+        if($res){ while($r=$res->fetch_assoc()){ $el.="<li><a href=\"{$r['url']}\" rel=\"nofollow noopener\" target=\"_blank\">{$r['title']}</a></li>"; } $res->close(); }
+        $vars['{EXTERNAL_LINKS}']=$el;
+        $db->close();
+    } else {
+        $vars['{INTERNAL_LINKS}']='';
+        $vars['{EXTERNAL_LINKS}']='';
+    }
     return strtr($template,$vars);
+}
+
+function connect_local_prompt(){
+    $cfgPath=__DIR__.'/local_config.secure';
+    if(!file_exists($cfgPath)) return array(null,null);
+    $cfg=json_decode(file_get_contents($cfgPath),true);
+    if(!$cfg) return array(null,null);
+    $db=@new mysqli($cfg['host'],$cfg['user'],$cfg['pass'],$cfg['name']);
+    if($db->connect_errno) return array(null,null);
+    $db->set_charset('utf8mb4');
+    return array($db,$cfg['prefix']);
 }
 ?>
